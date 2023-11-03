9e4f97a93e869bf7ce8efc577cc6ea4f9e650632
vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-11-03 14:30:53 -0300

    Iniciando função para adicionar medicamentos em andamento

--- model/administracao_de_medicamentos.py
+++ model/administracao_de_medicamentos.py
@@ -4,50 +4,53 @@ import json
 
 class AdmMedicamentos:
     def __init__(self):
-        self.caminho_arquivo_medicamentos = "suri/database/medicamentos.csv"
+        self.caminho_arquivo_medicamentos = "database/medicamentos.csv"
 
         if not os.path.exists(self.caminho_arquivo_medicamentos):
             arquivo = pandas.DataFrame()
-            arquivo.to_csv(self.caminho_arquivo_medicamentos, index=False)
+            arquivo.to_csv(self.caminho_arquivo_medicamentos, index=False) 
           
-    def cadastrar_medicamento(self, nome, principio_ativo, dosagem, forma_adiministracao, lote, quantidade, validade, fornecedor):
+    def cadastrar_medicamento(self, nome, principio_ativo, dosagem, forma_adiministracao):
         try:
             arquivo = pandas.read_csv(self.caminho_arquivo_medicamentos)
         except:
             arquivo = pandas.DataFrame()
-        
-        arquivo = arquivo.to_dict()
-        
-        
-        if nome in arquivo:
-            if lote in arquivo[nome]:
-                print("tudo encontrado!!")
-            else: 
-                print("nome encontrado, lote não")
+       
+        if not arquivo.empty and nome in arquivo['nome'].values:    
+            print("Medicamento já cadastrado!!")
         else:
-            novo_medicamento = {"principio_ativo": principio_ativo, 
-                             "dosagem": dosagem, 
-                             "forma_adiministracao": forma_adiministracao,
-                             "lotes": {lote: [quantidade, validade, fornecedor]}}
-            
+            novo_medicamento = {'nome': [nome],
+                                "principio_ativo": [principio_ativo], 
+                                "dosagem": [dosagem], 
+                                "forma_adiministracao": [forma_adiministracao],
+                                }  
             novo_medicamento = pandas.DataFrame(novo_medicamento)
-            arquivo = pandas.DataFrame(arquivo)
-            novo_medicamento.to_csv(self.caminho_arquivo_medicamentos, index=False, mode="a")
+            if arquivo.empty:
+                novo_medicamento.to_csv(self.caminho_arquivo_medicamentos, index=False, mode="a")
+            else:
+                novo_medicamento.to_csv(self.caminho_arquivo_medicamentos, index=False, mode="a", header=False)
 
-            print("nada encontrado")
+    def adicionar_lote(self, lote, quantidade, validade, fornecedor):
+        try:
+            arquivo = pandas.read_csv(self.caminho_arquivo_medicamentos)
+        except:
+            arquivo = pandas.DataFrame()
+
+        if not arquivo.empty:
+            print("Selecione o medicamento: ")
+            for medicamentos in arquivo['nome'].values:
+                print(medicamentos)
+        else:
+            print("Sem medicamentos registrados!")
 
         
         
     
 
-        
-
-        
-
-        
-
 adiministrar_medicamento = AdmMedicamentos()
-adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "654235485", 25, "14/07/2024", "GRFJE")
-adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "654235485", 25, "14/07/2024", "GRFJE")
-adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "123456789", 25, "14/07/2024", "GRFJE")
-adiministrar_medicamento.cadastrar_medicamento("remedio 2", "ingerir 2", "200ml 2", "tomar 2", "643516345", 19, "21/02/2024", "KMWXU")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar")
+adiministrar_medicamento.cadastrar_medicamento("remedio 2", "ingerir 2", "200ml 2", "tomar 2")
+
+adiministrar_medicamento.adicionar_lote('253255', 4365, '14/03/2024', 'NEUXFJ')
