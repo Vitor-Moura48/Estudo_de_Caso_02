8f438126e5b9b57e3495c43da43d923d909ece6d
vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-11-03 12:53:38 -0300

    adicionando informações do lote dos medicamentos (em andamento)

--- suri/model/administracao_de_medicamentos.py
+++ suri/model/administracao_de_medicamentos.py
@@ -1,25 +1,45 @@
 import pandas
 import os
+import json
 
 class AdmMedicamentos:
     def __init__(self):
-        self.caminho_arquivo = "suri/database/medicamentos.csv"
-        if not os.path.exists(self.caminho_arquivo):
+        self.caminho_arquivo_medicamentos = "suri/database/medicamentos.csv"
+
+        if not os.path.exists(self.caminho_arquivo_medicamentos):
             arquivo = pandas.DataFrame()
-            arquivo.to_csv(self.caminho_arquivo, index=False)
+            arquivo.to_csv(self.caminho_arquivo_medicamentos, index=False)
           
-    def cadastrar_medicamento(self, nome, principio_ativo, dosagem, forma_adiministracao):
+    def cadastrar_medicamento(self, nome, principio_ativo, dosagem, forma_adiministracao, lote, quantidade, validade, fornecedor):
         try:
-            arquivo = pandas.read_csv(self.caminho_arquivo)
+            arquivo = pandas.read_csv(self.caminho_arquivo_medicamentos)
         except:
             arquivo = pandas.DataFrame()
-
-        if nome in arquivo.columns:
-            print("medicamento já cadastrado!")
+        
+        arquivo = arquivo.to_dict()
+        
+        
+        if nome in arquivo:
+            if lote in arquivo[nome]:
+                print("tudo encontrado!!")
+            else: 
+                print("nome encontrado, lote não")
         else:
-            arquivo[nome] = [principio_ativo, dosagem, forma_adiministracao]
+            novo_medicamento = {"principio_ativo": principio_ativo, 
+                             "dosagem": dosagem, 
+                             "forma_adiministracao": forma_adiministracao,
+                             "lotes": {lote: [quantidade, validade, fornecedor]}}
+            
+            novo_medicamento = pandas.DataFrame(novo_medicamento)
+            arquivo = pandas.DataFrame(arquivo)
+            novo_medicamento.to_csv(self.caminho_arquivo_medicamentos, index=False, mode="a")
+
+            print("nada encontrado")
+
+        
+        
     
-        arquivo.to_csv(self.caminho_arquivo, index=False)
+
         
 
         
@@ -27,5 +47,7 @@ class AdmMedicamentos:
         
 
 adiministrar_medicamento = AdmMedicamentos()
-adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar")
-adiministrar_medicamento.cadastrar_medicamento("remedio 2", "ingerir 2", "200ml 2", "tomar 2")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "654235485", 25, "14/07/2024", "GRFJE")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "654235485", 25, "14/07/2024", "GRFJE")
+adiministrar_medicamento.cadastrar_medicamento("remedio", "ingerir", "200ml", "tomar", "123456789", 25, "14/07/2024", "GRFJE")
+adiministrar_medicamento.cadastrar_medicamento("remedio 2", "ingerir 2", "200ml 2", "tomar 2", "643516345", 19, "21/02/2024", "KMWXU")
