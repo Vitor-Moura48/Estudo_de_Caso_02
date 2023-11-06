9123a9a143875616c5f1986cee7b221f7fdc6143
Ott-on <ottonvinicius1004@gmail.com>
2023-11-06 11:39:40 -0300

    Update_02

--- model/cadastro_paciente.py
+++ model/cadastro_paciente.py
@@ -12,14 +12,14 @@ def adicionar_data():
 def cadastrar_paciente():
     while True:
         novo_paciente = {}
-        novo_paciente['Data de Criação'] = adicionar_data()
+        novo_paciente['Data de Criacao'] = adicionar_data()
         novo_paciente['Nome'] = input('Digite o nome do paciente: ')
         novo_paciente['Idade'] = input('Digite a idade do paciente: ')
         novo_paciente['Sexo'] = input('Digite o sexo do paciente: ')
         novo_paciente['Identidade'] = input('Digite a identidade do paciente: ')
-        novo_paciente['Contato de Emergência'] = input('Digite um contato para emergência: ')
+        novo_paciente['Contato de Emergencia'] = input('Digite um contato para emergência: ')
         novo_paciente['Estado'] = input('Digite o estado do paciente (Leve, Moderado ou Grave): ')
-        novo_paciente['Prontuário'] = []
+        novo_paciente['prontuario'] = []
 
         lista_de_todos_os_pacientes.append(novo_paciente)
 
@@ -36,18 +36,18 @@ def cadastrar_paciente():
         if opcao.lower() != 's':
             break
 
-# Função para cadastrar prontuário do paciente
+# Função para cadastrar prontuario do paciente
 def cadastrar_prontuario(paciente):
     while True:
         prontuario = {}
-        prontuario['Data do Diagnóstico'] = adicionar_data()
-        prontuario['Diagnóstico'] = input('Digite o diagnóstico do paciente: ')
-        prontuario['Condição'] = input('Digite a condição do paciente: ')
-        prontuario['Recomendação de Medicação'] = input('Digite a recomendação de medicação: ')
-        prontuario['Histórico Médico'] = input('Digite o histórico médico (opcional): ')
+        prontuario['Data do diagnostico'] = adicionar_data()
+        prontuario['diagnostico'] = input('Digite o diagnostico do paciente: ')
+        prontuario['condicao'] = input('Digite a condicao do paciente: ')
+        prontuario['Recomendacao de Medicacao'] = input('Digite a Recomendacao de Medicacao: ')
+        prontuario['Historico Medico'] = input('Digite o Historico Medico (opcional): ')
         prontuario['Exames'] = input('Digite os exames (opcional): ')
 
-        paciente['Prontuário'].append(prontuario)
+        paciente['prontuario'].append(prontuario)
 
         
         nome_paciente = paciente['Nome']
@@ -57,44 +57,44 @@ def cadastrar_prontuario(paciente):
                 writer.writeheader()
             writer.writerow(prontuario)
 
-        print('Prontuário cadastrado com sucesso!\n')
+        print('prontuario cadastrado com sucesso!\n')
 
-        opcao = input('Deseja cadastrar outro prontuário para o paciente? (S/N): ')
+        opcao = input('Deseja cadastrar outro prontuario para o paciente? (S/N): ')
         if opcao.lower() != 's':
             break
 
 def listar_pacientes_e_prontuarios(pacientes):
     for i, paciente in enumerate(pacientes):
         print(f'\n** Paciente {i + 1} **')
-        print(f'Data de Criação: {paciente["Data de Criação"]}')
+        print(f'Data de Criacao: {paciente["Data de Criacao"]}')
         print(f'Nome: {paciente["Nome"]}')
         print(f'Idade: {paciente["Idade"]}')
         print(f'Sexo: {paciente["Sexo"]}')
         print(f'Identidade: {paciente["Identidade"]}')
-        print(f'Contato de Emergência: {paciente["Contato de Emergência"]}')
+        print(f'Contato de Emergencia: {paciente["Contato de Emergencia"]}')
         print(f'Estado: {paciente["Estado"]}')
-        print('\n** Prontuários **')
-        prontuarios = paciente["Prontuário"]
+        print('\n** prontuarios **')
+        prontuarios = paciente["prontuario"]
         if prontuarios:
             for j, prontuario in enumerate(prontuarios):
-                print(f'** Prontuário {j + 1} **')
-                print(f'Data do Diagnóstico: {prontuario["Data do Diagnóstico"]}')
-                print(f'Diagnóstico: {prontuario["Diagnóstico"]}')
-                print(f'Condição: {prontuario["Condição"]}')
-                print(f'Recomendação de Medicação: {prontuario["Recomendação de Medicação"]}')
-                if prontuario['Histórico Médico']:
-                    print(f'Histórico Médico: {prontuario["Histórico Médico"]}')
+                print(f'** prontuario {j + 1} **')
+                print(f'Data do diagnostico: {prontuario["Data do diagnostico"]}')
+                print(f'diagnostico: {prontuario["diagnostico"]}')
+                print(f'condicao: {prontuario["condicao"]}')
+                print(f'Recomendacao de Medicacao: {prontuario["Recomendacao de Medicacao"]}')
+                if prontuario['Historico Medico']:
+                    print(f'Historico Medico: {prontuario["Historico Medico"]}')
                 if prontuario['Exames']:
                     print(f'Exames: {prontuario["Exames"]}')
         else:
-            print('Nenhum prontuário cadastrado para este paciente.')
+            print('Nenhum prontuario cadastrado para este paciente.')
 
 if __name__ == "__main__":
     while True:
         print('\nEscolha uma opção:')
         print('1 - Cadastrar novo paciente')
-        print('2 - Cadastrar prontuário para paciente existente')
-        print('3 - Listar pacientes e prontuários')
+        print('2 - Cadastrar prontuario para paciente existente')
+        print('3 - Listar pacientes e prontuarios')
         print('4 - Sair')
 
         opcao = input('Opção: ')
@@ -103,7 +103,7 @@ if __name__ == "__main__":
             cadastrar_paciente()
         elif opcao == '2':
             listar_pacientes_e_prontuarios(lista_de_todos_os_pacientes)
-            nome_paciente = input('Digite o nome do paciente para cadastrar um prontuário: ')
+            nome_paciente = input('Digite o nome do paciente para cadastrar um prontuario: ')
             paciente_encontrado = next((p for p in lista_de_todos_os_pacientes if p['Nome'] == nome_paciente), None)
             if paciente_encontrado:
                 cadastrar_prontuario(paciente_encontrado)
