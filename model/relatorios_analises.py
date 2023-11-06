2a48e798549b6aeb8f1ed10fff91e084f2e3f3bd
vitor Gabriel <vitor_gabriel.4815.7@gmail.com>
2023-11-06 20:50:08 -0300

    criando estrutura inicial do módulo 08

---
+++ model/relatorios_analises.py
@@ -0,0 +1,20 @@
+import pandas
+
+class RelatorioAnalise:
+    def __init__(self):
+        pass
+
+    def gerar_relatorio_txt(self):
+        pass
+    
+    def gerar_relatorio_csv(self):
+        pass
+    
+    def metricas_de_ocupacao(self):
+        pass
+
+    def eficiencia_uso_equipamentos(self):
+        pass
+
+    def dados_atendimento_paciente(self):
+        pass
